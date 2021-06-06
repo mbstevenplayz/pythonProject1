3f85aac8a662140361b75768fcc9c82fd8ceaea7
stevenplayz <bmsbenmirasteve@gmail.com>
2021-06-06 19:11:41 +0800

    video number 2 and 3

---
+++ 003_Comments.py
@@ -0,0 +1,10 @@
+# this is a comment
+print ("Hello StevenPlayz")
+
+print("another way to put a comment") # this line is for printing
+
+# this
+# is
+# multiple line comment
+print("the above lines are my multiple comments")
+
