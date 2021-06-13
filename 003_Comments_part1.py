bbffe79ff2797e63487ae91ab928bcd0c82c5c7b
stevenplayz <bmsbenmirasteve@gmail.com>
2021-06-13 14:38:36 +0800

    video number 3 to 5

---
+++ 003_Comments_part1.py
@@ -0,0 +1,13 @@
+# the line below will print Hello StevenPlayz
+print ("Hello StevenPlayz")
+
+print("another way to put a comment") # this line will print another way to put a comment
+
+# this
+# is
+# multiple line comment
+print("the above lines are my multiple comments")
+
+
+# if you don't want to run the print
+# print("hello Steven")
---
+++ 004_Comments_part2.py
@@ -0,0 +1,8 @@
+"""
+this can be considered
+multiple line
+comments.
+a string that is not assigned to a variable
+"""
+
+print("the unassigned string above can be used for multiple line comments")
